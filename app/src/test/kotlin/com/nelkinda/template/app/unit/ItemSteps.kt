b9ca48bcd19869631ab1f35594bd675ed5ef8303
neo <bhaveshsalunke14@gmail.com>
2021-03-17 00:26:17 +0530

    RG | Test item equality and inequality

--- app/src/test/kotlin/com/nelkinda/template/app/unit/ItemSteps.kt
+++ app/src/test/kotlin/com/nelkinda/template/app/unit/ItemSteps.kt
@@ -1,7 +1,9 @@
 package com.nelkinda.template.app.unit
 
 import com.nelkinda.template.app.model.Item
+import io.cucumber.datatable.DataTable
 import io.cucumber.java.en.And
+import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
 import org.junit.Assert.*
@@ -9,6 +11,8 @@ import org.junit.Assert.*
 class ItemSteps {
 
     private lateinit var item: Item
+    private lateinit var item1: Item
+    private lateinit var item2: Item
 
     @When("An item is created with name as {string}, quantity as {int}, unit price as {double}")
     fun `An item is created with`(name: String, count: Int, price: Double){
@@ -28,6 +32,25 @@ class ItemSteps {
     @And("The price of Item should be {double}")
     fun `The price of Item should be`(price: Double){
         assertEquals(price.toBigDecimal(), item.price)
+    }
+
+    @Given("Two item with name, quantity and price are as follows:")
+    fun `Two item with name, quantity and price are`(dataTable: DataTable){
+        val map1 = dataTable.asMaps()[0]
+        item1 = Item(map1["Item"]!!, map1["Count"]!!.toInt(), map1["Price"]!!.toBigDecimal())
+
+        val map2 = dataTable.asMaps()[1]
+        item2 = Item(map2["Item"]!!, map2["Count"]!!.toInt(), map2["Price"]!!.toBigDecimal())
+    }
+
+    @Then("Both items should be equal")
+    fun `Both item should be equal`(){
+        assertEquals(item1,item2)
+    }
+
+    @Then("Both items should not be equal")
+    fun `Both item should not be equal`(){
+        assertNotEquals(item1,item2)
     }
 
 }
--- app/src/test/resources/features/Item.feature
+++ app/src/test/resources/features/Item.feature
@@ -5,3 +5,17 @@ Feature: Item
     Then The name of Item should be "Book"
     And The count of Item should be 1
     And The price of Item should be 15.54
+
+  Scenario: Two items with known properties
+    Given Two item with name, quantity and price are as follows:
+      | Item | Count | Price |
+      | Book | 1     | 12.49 |
+      | Book | 1     | 12.49 |
+    Then Both items should be equal
+
+    Given Two item with name, quantity and price are as follows:
+      | Item | Count | Price |
+      | Toy  | 3     | 2.49  |
+      | Book | 1     | 12.49 |
+    Then Both items should not be equal
+
